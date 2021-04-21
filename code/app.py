cadff6410b68d9492acd62dedeca971c28fdc1d9
Akira Takada <takadaa@tcnj.edu>
2021-04-21 17:27:42 -0400

    initial push for application

---
+++ code/app.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python3
+
+"""
+ONE-TIME SETUP
+
+To run this example in the CSC 315 VM you first need to make
+the following one-time configuration changes:
+
+# set the postgreSQL password for user 'lion'
+sudo -u postgres psql
+    ALTER USER lion PASSWORD 'lion';
+    \q
+
+# install pip for Python 3
+sudo apt update
+sudo apt install python3-pip
+
+# install psycopg2
+pip3 install psycopg2-binary
+
+# install flask
+pip3 install flask
+
+# logout, then login again to inherit new shell environment
+"""
+
+"""
+CSC 315
+Spring 2021
+John DeGood
+
+# usage
+export FLASK_APP=app.py 
+flask run
+
+# then browse to http://127.0.0.1:5000/
+
+Purpose:
+Demonstrate Flask/Python to PostgreSQL using the psycopg adapter.
+Connects to the 7dbs database from "Seven Databases in Seven Days"
+in the CSC 315 VM.
+
+For psycopg documentation:
+https://www.psycopg.org/
+
+This example code is derived from:
+https://www.postgresqltutorial.com/postgresql-python/
+https://scoutapm.com/blog/python-flask-tutorial-getting-started-with-flask
+https://www.geeksforgeeks.org/python-using-for-loop-in-flask/
+"""
+
+import psycopg2
+from config import config
+from flask import Flask, render_template, request
+ 
+# app.py
+
+app = Flask(__name__)
+
+# psql queries 
+
+# serve form web page
+@app.route("/")
+def form():
+    return render_template('my-form.html')
+
+# handle form data
+@app.route('/form-handler', methods=['POST'])
+def handle_data():
+    rows = connect(request.form['query'])
+
+    return render_template('my-result.html', rows=rows)
+
+if __name__ == '__main__':
+    app.run(debug = True)
