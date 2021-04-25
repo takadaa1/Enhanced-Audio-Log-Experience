e7f1de8cfa4902807d98a040c32707983e360c89
Akira Takada <takadaa@tcnj.edu>
2021-04-24 22:44:56 -0400

    testing query

--- code/app.py
+++ code/app.py
@@ -1,63 +1,30 @@
 #! /usr/bin/python3
 
-"""
-ONE-TIME SETUP
-
-To run this example in the CSC 315 VM you first need to make
-the following one-time configuration changes:
-
-# set the postgreSQL password for user 'lion'
-sudo -u postgres psql
-    ALTER USER lion PASSWORD 'lion';
-    \q
-
-# install pip for Python 3
-sudo apt update
-sudo apt install python3-pip
-
-# install psycopg2
-pip3 install psycopg2-binary
-
-# install flask
-pip3 install flask
-
-# logout, then login again to inherit new shell environment
-"""
-
-"""
-CSC 315
-Spring 2021
-John DeGood
-
-# usage
-export FLASK_APP=app.py 
-flask run
-
-# then browse to http://127.0.0.1:5000/
-
-Purpose:
-Demonstrate Flask/Python to PostgreSQL using the psycopg adapter.
-Connects to the 7dbs database from "Seven Databases in Seven Days"
-in the CSC 315 VM.
-
-For psycopg documentation:
-https://www.psycopg.org/
-
-This example code is derived from:
-https://www.postgresqltutorial.com/postgresql-python/
-https://scoutapm.com/blog/python-flask-tutorial-getting-started-with-flask
-https://www.geeksforgeeks.org/python-using-for-loop-in-flask/
-"""
 
 import psycopg2
 from config import config
 from flask import Flask, render_template, request
  
+# Database creds
+user = 'lion'
+passw = 'lion'
+db = 'proj'
+
+
 # app.py
 
 app = Flask(__name__)
 
-# psql queries 
+# main index page
+@app.route('/')
+def index():
+    conn = psycopg2.connect("dbname=" + db + " user=" + user + " password=" + passw)
+    curr = conn.cursor()
+    curr.execute("""SELECT title FROM interview order by title""");
+    titles = curr.fetchall()
+    curr.close()
+
+    return render_template('index.html', titles=titles)
 
 # serve form web page
 @app.route("/")
